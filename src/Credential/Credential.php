318d483fce6f704c4e9a749a10589f162bc923aa
gertoska <tosk1@protonmail.com>
2017-08-27 11:35:40 +0200

    testing styleci

--- src/Credential/Credential.php
+++ src/Credential/Credential.php
@@ -3,7 +3,8 @@
 namespace Gertoska\OAuth2Request\Credential;
 
 /**
- * Class Credential.
+ * Class Credential
+ * @package Gertoska\OAuth2Request\Credential
  */
 class Credential
 {
@@ -65,17 +66,17 @@ class Credential
     /**
      * AccessData constructor.
      *
-     * @param string      $uri
-     * @param string      $authorization
-     * @param string      $grantType
-     * @param string      $username
-     * @param string      $password
+     * @param string $uri
+     * @param string $authorization
+     * @param string $grantType
+     * @param string $username
+     * @param string $password
      * @param string|null $accessToken
      * @param string|null $tokenType
      * @param string|null $refreshToken
-     * @param int|null    $expiresIn
+     * @param int|null $expiresIn
      * @param string|null $scope
-     * @param int|null    $obtainedIn
+     * @param int|null $obtainedIn
      */
     public function __construct(
         string $uri,
@@ -195,9 +196,9 @@ class Credential
      * @param string $accessToken
      * @param string $tokenType
      * @param string $refreshToken
-     * @param int    $expiresIn
+     * @param int $expiresIn
      * @param string $scope
-     * @param int    $obtainedIn
+     * @param int $obtainedIn
      *
      * @return Credential
      */
@@ -224,7 +225,7 @@ class Credential
      */
     public function checkIfIsNotNecessaryToRefreshToken()
     {
-        return null !== $this->accessToken() && !$this->hasExpired();
+        return (null !== $this->accessToken() && !$this->hasExpired());
     }
 
     /**
@@ -232,6 +233,6 @@ class Credential
      */
     public function hasExpired()
     {
-        return null === $this->expiresIn || microtime(true) - $this->obtainedIn >= $this->expiresIn;
+        return (null === $this->expiresIn || microtime(true) - $this->obtainedIn >= $this->expiresIn);
     }
 }
